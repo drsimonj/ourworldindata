706417393a2585ed9d323cfbad14173f5aaed55d
drsimonj <drsimonjackson@gmail.com>
2016-09-27 22:24:51 +1000

    Add data set, mortality: child mortality v healthcare expenditure

---
+++ R/mortality.R
@@ -0,0 +1,15 @@
+#' Child Mortality vs. Total healthcare expenditure per capita
+#'
+#' Child Mortality (0-5 year-olds dying per 1,000 born) vs. Total healthcare
+#' expenditure per capita (PPP).
+#'
+#' @format A data frame with 33,274 rows and 5 variables:
+#' \describe{
+#'   \item{country}{Country}
+#'   \item{year}{Year}
+#'   \item{health_exp}{Health Expenditure per capita (WDI)}
+#'   \item{child_mort}{Child mortality (Gapminder)}
+#'   \item{continent}{Continent}
+#' }
+#' @source \url{https://ourworldindata.org/grapher/child-mortality-vs-health-expenditure?tab=data}
+"mortality"
