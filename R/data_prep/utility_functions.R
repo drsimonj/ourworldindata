2de2182178c579f4f16b445593b25b7ec77f35c6
drsimonj <drsimonjackson@gmail.com>
2016-10-01 14:02:49 +1000

    Clean utility function nest_duplicated()

--- R/data_prep/utility_functions.R
+++ R/data_prep/utility_functions.R
@@ -3,19 +3,16 @@
 # Args:
 #   df Data frame of joined data frames with duplicated columns.
 #   suffixes Character string to match suffixes. E.g., the default "\\.[xy]"
-#            finds any columnsending with .x or .y
+#            finds any columns ending with .x or .y
 nest_duplicated <- function(df, suffixes = "\\.[xy]") {
   #library(tidyverse)
   #library(stringr)
-
-  multi_suffix <- stringr::str_c("(", suffixes, ")+")
 
   # Search string to match any duplicated variables
   search_string <- df %>%
                      dplyr::select(dplyr::matches(suffixes)) %>%
                      names() %>%
                      stringr::str_replace_all(suffixes, "") %>%
-                     #stringr::str_replace(multi_suffix, "") %>%
                      unique() %>%
                      stringr::str_c(collapse = "|") %>%
                      stringr::str_c("(", ., ")($|", suffixes, ")")
@@ -24,7 +21,6 @@ nest_duplicated <- function(df, suffixes = "\\.[xy]") {
   df <- df %>%
           tidyr::gather(variable, value, dplyr::matches(search_string)) %>%
           dplyr::mutate(variable = stringr::str_replace_all(variable, suffixes, ""))
-          #dplyr::mutate(variable = stringr::str_replace(variable, multi_suffix, ""))
 
   # Group by all columns except value to convert duplicated rows into list, then
   # spread by variable (var)
